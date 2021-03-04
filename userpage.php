4c12fcd00baaa603f1694b8114c7fdbdd309fea7
Mohammed Zishan <mohdzish2000@gmail.com>
2021-03-04 23:46:43 +0100

    Add files via upload

--- userpage.php
+++ userpage.php
@@ -1,4 +1,6 @@
-<?php include_once("navbar.php"); ?>
+<?php 
+session_start();
+include_once("navbar.php"); ?>
 
 <!doctype html>
 <html>
@@ -17,54 +19,7 @@
         <main role="main" class="col-md-8 ck-content">
 
            <div class=" align-items-center pt-3 pb-1 mb-3 border-bottom">
-
-              <div class="row">
-                <div class="col-md-9">
-                  <h1 class="username">Username</h1>
-                  <p class="lead postsubtitle">
-                    This User is a professional freelance writer with more than 20 years experience in the technology space. He’s written for Brainstorm, Website Compass, and Business Connections eNewsletter, as well as tech companies including HP, IBM, Intel, Samsung, and Amazon Web Services.
-                  </p>
-                </div>
-                <div class="col-md-3 mt-4">
-                  <img src="Images/UserAvatar/example3.png" class="avatarinpage  float-right" id="useravatardropdown" >
-                </div>
-              </div>
-              
-          </div>
-
-          <h1 class="yourarticle pt-3">Your articles</h1>
-
-          <div class="allarticles pt-3">
-            <div class="card articlecard">
-              <div class="row no-gutters">
-                <div class="col-md-9">
-                  <div class="card-body">
-                    <h5 class="card-title">How to fix windows not genuine error</h5>
-                    This User is a professional freelance writer with more than 20 years experience in the technology space. 
-                  </div>
-                </div>
-                <div class="col-md-2">
-                  <img class="  articlecardimg" src="Images/MainPage/temp3.png" alt="sans">
-                </div>
-              </div>
-          </div>
-
-          <div class="allarticles pt-3">
-            <div class="card articlecard">
-              <div class="row no-gutters">
-                <div class="col-md-9">
-                  <div class="card-body">
-                    <h5 class="card-title">How to fix windows not genuine error</h5>
-                    This User is a professional freelance writer with more than 20 years experience in the technology space. 
-                  </div>
-                </div>
-                <div class="col-md-2">
-                  <img class="  articlecardimg" src="Images/MainPage/temp3.png" alt="sans">
-                </div>
-              </div>
-          </div>
-
-          <?php 
+            <?php 
 
               $mysqli = new mysqli("localhost","root", "","fixcovery");
 
@@ -72,13 +27,68 @@
                 echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
                 exit();
               }
-
-              $result = mysqli_query($mysqli, "SELECT * FROM posts WHERE title='How to fix an error'");
-              $row = mysqli_fetch_assoc($result);
+              ?>
+              <div class="row">
+                
+                <div class="col-md-2 text-center mt-4 order-sm-12 ">
+                  <img src="Images/UserAvatar/example3.png" class="avatarinpage  float-md-right mx-auto" id="useravatardropdown" >
+                </div>
+                <div class="col-md-10 order-sm-1">
+                  <h1 class="username text-center text-md-left"><?php echo $_SESSION['username']; ?></h1>
+                  <p class="lead postsubtitle">
+                    I am a professional freelance writer with more than 20 years experience in the technology space. He’s written for Brainstorm, Website Compass, and Business Connections eNewsletter, as well as tech companies including HP, IBM, Intel, Samsung, and Amazon Web Services.
+                  </p>
+                </div>
+              </div>
               
-              echo $row['body'];
-              $mysqli -> close();
-           ?>
+          </div>
+
+          <h1 class="yourarticle pt-3">Your articles</h1>
+
+
+          <div class="allarticles pt-3">
+            <?php
+
+              $query = "SELECT * FROM posts where username='".$_SESSION['username']."'";
+              $result = $mysqli->query($query);
+              function seoUrl($string) {
+    //Lower case everything
+                  $string = strtolower($string);
+                  //Make alphanumeric (removes all other characters)
+                  $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+                  //Clean up multiple dashes or whitespaces
+                  $string = preg_replace("/[\s-]+/", " ", $string);
+                  //Convert whitespaces and underscore to dash
+                  $string = preg_replace("/[\s_]/", "-", $string);
+                  return $string;
+              }
+
+              if ($result->num_rows > 0) {
+                while($row = $result->fetch_array()){ ?>
+
+
+                  <div class="card shadow-sm  articlecard mb-4">
+                    <div class="row no-gutters">
+                      <div class="col-md-9">
+                        <div class="card-body">
+                          <a href="article/<?php echo $row['postid'] . '/'. seoUrl($row['title']);?>"><h5 class="card-title"><?php echo $row['title'] ?></h5></a>
+                          <?php echo substr($row['body'], 0, 100); ?>
+                        </div>
+                      </div>
+                      <div class="col-md-2">
+                        <img class="  articlecardimg" src="Images/MainPage/temp3.png" alt="sans">
+                      </div>
+                    </div>
+                  </div>
+
+
+               <?php }} else {?> 
+
+                  <p class="lead postsubtitle">
+                    There are no articles yet. Start writing here.
+                  </p>
+
+              <?php } $mysqli->close();?> 
          
         </main>
 
