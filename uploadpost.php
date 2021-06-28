04703ea2b90ee728a73b99ec6738d0b25a575eb2
Mohammed Zishan <mohdzish2000@gmail.com>
2021-06-29 00:20:12 +0200

    Add files via upload

--- uploadpost.php
+++ uploadpost.php
@@ -2,9 +2,9 @@
 
 session_start();
 if(!isset($_SESSION['verified']) || $_SESSION['verified'] !== true) {
-		header("Location: loginpage.php");
-		die();
-	}
+	header("Location: loginpage.php");
+	die();
+}
 
 
 include_once("navbar.php");
@@ -13,33 +13,45 @@ include_once("navbar.php");
 <!doctype html>
 <html>
 <head>
-  <link rel="stylesheet" href="uploadpost.css">
-  <link rel="stylesheet" href="ckeditor5/content-styles.css">
-  <script src="ckeditor5/ckeditor.js"></script>
+	<link rel="stylesheet" href="uploadpost.css">
+	<link rel="stylesheet" href="ckeditor5/content-styles.css">
+	<script src="ckeditor5/ckeditor.js"></script>
+
+	<link rel="stylesheet" href="Editor/css/medium-editor.css"> <!-- Core -->
+	<link rel="stylesheet" href="Editor/css/default.css"> <!-- or any other theme -->
+
+
 </head>
 
 <body>
 
 
-<script type="text/javascript">
+	<script type="text/javascript">
 
-	function descbuttonchange(descbutton) {
 		
-		descbutton.closest(".methodclass").querySelector("#metinput").value="";
-	  var x = document.getElementById("adddesc");
+
+var ckeditor = document.querySelectorAll('.editor p');
+[].forEach.call(links, function(item) {
+	item.classList.add('myClass');
+});
+
+function descbuttonchange(descbutton) {
+
+	descbutton.closest(".methodclass").querySelector("#metinput").value="";
+	var x = document.getElementById("adddesc");
 
 	  if (x.innerHTML.includes("Remove")) { //includes check if it's add or remove
-	    x.innerHTML = "<i class='fas fa-plus'></i> Add Description";
+	  	x.innerHTML = "<i class='fas fa-plus'></i> Add Description";
 		//x.style.cssText  = "border-color: #0275d8;";
 		descbutton.closest(".methodclass").querySelector("#metinput").value="";
-	  } else {
-	    x.innerHTML = "<i class='fas fa-times'></i> Remove Description";
-	    descbutton.closest(".methodclass").querySelector("#metinput").value="";
+	} else {
+		x.innerHTML = "<i class='fas fa-times'></i> Remove Description";
+		descbutton.closest(".methodclass").querySelector("#metinput").value="";
 	    //x.style.cssText  = "border-color: #c21919; color:#c21919;";
-	  }
 	}
+}
 
-	function videobtnchange(addvideobutton) {
+function videobtnchange(addvideobutton) {
 	 /* var x = document.getElementById("addvideo");
 
 	  if (x.innerHTML.includes("Remove")) { //includes check if it's add or remove
@@ -50,10 +62,10 @@ include_once("navbar.php");
 	    x.innerHTML = "<i class='fab fa-youtube'></i> Remove Video";
 	    document.getElementById("input").value = "";
 	    //x.style.cssText  = "border-color: #c21919; color:#c21919;";
-	  }*/
-	}
+	}*/
+}
 
-	function searchvideourl(){
+function searchvideourl(){
 		//var videospot = document.getElementsByClassName("videocanvas");
 		var urlinput = document.getElementById("videourlinput").value;
 		
@@ -61,24 +73,24 @@ include_once("navbar.php");
 		var embedurl = urlinput.replace("watch?v=", "embed/");
 		alert(embedurl);
 		var videospot = document.getElementById('videocanvas');
-  		videospot.innerHTML = '<iframe height="200px" width="auto" src="'+ embedurl + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen SameSite="None"></iframe>';
+		videospot.innerHTML = '<iframe height="200px" width="auto" src="'+ embedurl + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen SameSite="None"></iframe>';
 	}
 
 	function stepadd(addbutton) {
-	  
+
 	    //let parentdiv = theaddbutton.parentNode.parentNode.parentNode.parentNode.parentNode;
 
 	    var thismethod = addbutton.closest(".methodbox");
 
 	    let menu = thismethod.querySelector('.stepclass');
-        let clonedMenu = menu.cloneNode(true);
+	    let clonedMenu = menu.cloneNode(true);
 
 	    var numofsteps = thismethod.querySelectorAll(".stepclass");
 	    var maximumstepcount = parseInt(numofsteps.length, 10)
 
 		//clonedMenu.id = 'stepblock' + (maximumstepcount+1);
 
-        thismethod.querySelector(".forsteps").appendChild(clonedMenu);
+		thismethod.querySelector(".forsteps").appendChild(clonedMenu);
 
         //change step number in each repetition
         var changestepnumber = clonedMenu.getElementsByClassName("stepcircle")[0];
@@ -101,7 +113,7 @@ include_once("navbar.php");
         removestepbtn.disabled = false;
         removestepbtn.id = "removebuttonid" + (maximumstepcount+1);
 
-	}
+    }
 
 	function removestep(removebutton) {  //id is for which step number is selected to remove
 		
@@ -117,34 +129,34 @@ include_once("navbar.php");
 	function stepfixer(forsteps){
 
 		var numofsteps = forsteps.querySelectorAll(".stepclass");
-	    var maximumstepcount = parseInt(numofsteps.length, 10)
+		var maximumstepcount = parseInt(numofsteps.length, 10)
 		
 
-	    for (j=1; j < maximumstepcount; j++) {
+		for (j=1; j < maximumstepcount; j++) {
 
 	    	//Fixing the step display number
 	    	var newelement = numofsteps[j].getElementsByClassName("stepcircle")[0];
-            newelement.innerHTML = j+1;
+	    	newelement.innerHTML = j+1;
 
 	    	//Fixing the remove button id 
-            var changebuttonid = numofsteps[j].querySelector(".removestepbtn");
-          	changebuttonid.id = "removebuttonid" + (j+1);
+	    	var changebuttonid = numofsteps[j].querySelector(".removestepbtn");
+	    	changebuttonid.id = "removebuttonid" + (j+1);
 
 	    	//Fixing the step number inside input
-          	var changeinnerstep = numofsteps[j].querySelector(".titlelabel");
-          	changeinnerstep.innerHTML = "Step " + (j+1);
+	    	var changeinnerstep = numofsteps[j].querySelector(".titlelabel");
+	    	changeinnerstep.innerHTML = "Step " + (j+1);
 	    	//alert("numofsteps[j].id")
-		}
+	    }
 	}
 	
 	function noteadd(thenotebutton) {
 
 		var thismethod = thenotebutton.closest(".methodbox");
-	  	let menu = thismethod.querySelector('#notebox');
-        let clonedMenu = menu.cloneNode(true);
-        clonedMenu.hidden = false;
+		let menu = thismethod.querySelector('#notebox');
+		let clonedMenu = menu.cloneNode(true);
+		clonedMenu.hidden = false;
 
-        thismethod.querySelector(".forsteps").appendChild(clonedMenu);
+		thismethod.querySelector(".forsteps").appendChild(clonedMenu);
 	}
 
 	function removenote(thenotebutton) {  
@@ -157,12 +169,12 @@ include_once("navbar.php");
 
 	function addimage(theimagebutton){
 		var thismethod = theimagebutton.closest(".methodbox");
-	  	let menu = document.querySelector('#imagebox');
-        let clonedMenu = menu.cloneNode(true);
-        clonedMenu.hidden = false;
-        clonedMenu.tag = "";
+		let menu = document.querySelector('#imagebox');
+		let clonedMenu = menu.cloneNode(true);
+		clonedMenu.hidden = false;
+		clonedMenu.tag = "";
 
-        thismethod.querySelector(".forsteps").appendChild(clonedMenu);
+		thismethod.querySelector(".forsteps").appendChild(clonedMenu);
 	}
 
 	function removeimage(removeimagebutton){
@@ -174,23 +186,23 @@ include_once("navbar.php");
 
 	function preview_image(event, theimagebutton) 
 	{
-	 var reader = new FileReader();
-	 reader.onload = function()
-	 {
-	 	var output = theimagebutton.parentNode.parentNode.querySelector("#output_image")
-	 	output.src = reader.result;
-	 }
-	 reader.readAsDataURL(event.target.files[0]);
+		var reader = new FileReader();
+		reader.onload = function()
+		{
+			var output = theimagebutton.parentNode.parentNode.querySelector("#output_image")
+			output.src = reader.result;
+		}
+		reader.readAsDataURL(event.target.files[0]);
 	}
 
 	function getfinalstring(){
 		
 		//check number of methods to use in for loop
 		var numofmethods = document.querySelectorAll(".methodclass");
-	    var maximummethodcount = parseInt(numofmethods.length, 10);
-	    var finalstring = "";
+		var maximummethodcount = parseInt(numofmethods.length, 10);
+		var finalstring = "";
 
-	for (methodnumber = 1; methodnumber < maximummethodcount; methodnumber++){
+		for (methodnumber = 1; methodnumber < maximummethodcount; methodnumber++){
 		var divMethod = document.querySelectorAll(".methodclass")[methodnumber]; //THE method
 		var divElem = divMethod.querySelectorAll("#forsteps")[0];
 
@@ -226,142 +238,158 @@ include_once("navbar.php");
 			finalstring = finalstring + temparray[sentence];
 		}
 		
-		}
+	}
 
-		alert(finalstring);
-		
+	alert(finalstring);
+
+}
+
+
+function methodadd() {
+	let menu = document.querySelector('#methodblock');
+	let clonedMenu = menu.cloneNode(true);
+
+	clonedMenu.hidden = false;
+
+	var numofsteps = document.querySelectorAll(".methodclass");
+	var maximummethodcount = parseInt(numofsteps.length, 10);
+
+	clonedMenu.id = 'methodblock' + (maximummethodcount+1);
+	document.getElementById("formethod").appendChild(clonedMenu);
+
+	clonedMenu.querySelector("#methoddescbox").id = "methoddescbox" + (maximummethodcount+1);
+
+	clonedMenu.querySelector(".removemethodbtn").disabled = false;
+
+}
+
+function removemethod(removemethodbtn){
+	if (confirm("Delete this method?")) {
+		var themethodtoremove = removemethodbtn.parentNode.parentNode;
+		removemethodbtn.parentNode.parentNode.parentNode.removeChild(themethodtoremove)
+	} 
+	else {
+
 	}
 
 
-	function methodadd() {
-	  let menu = document.querySelector('#methodblock');
-        let clonedMenu = menu.cloneNode(true);
+}
 
-        clonedMenu.hidden = false;
+function loadmethod() {
+	let menu = document.querySelector('#methodblock');
+	let clonedMenu = menu.cloneNode(true);
+	clonedMenu.hidden = false;
 
-        var numofsteps = document.querySelectorAll(".methodclass");
-	    var maximummethodcount = parseInt(numofsteps.length, 10);
+	var addstepbutton = clonedMenu.querySelector("#addstepbtn0")
+	addstepbutton.id = "addstepbutton1"
 
-	    clonedMenu.id = 'methodblock' + (maximummethodcount+1);
-        document.getElementById("formethod").appendChild(clonedMenu);
+	clonedMenu.id = 'methodblock1';
+	document.getElementById("formethod").appendChild(clonedMenu);
+}
 
-        clonedMenu.querySelector("#methoddescbox").id = "methoddescbox" + (maximummethodcount+1);
-
-        clonedMenu.querySelector(".removemethodbtn").disabled = false;
-
-	}
-
-	function removemethod(removemethodbtn){
-		if (confirm("Delete this method?")) {
-		    var themethodtoremove = removemethodbtn.parentNode.parentNode;
-			removemethodbtn.parentNode.parentNode.parentNode.removeChild(themethodtoremove)
-		  } 
-		else {
-
-		  }
-		
-
-	}
-
-	function loadmethod() {
-        let menu = document.querySelector('#methodblock');
-        let clonedMenu = menu.cloneNode(true);
-        clonedMenu.hidden = false;
-
-        var addstepbutton = clonedMenu.querySelector("#addstepbtn0")
-        addstepbutton.id = "addstepbutton1"
-
-	    clonedMenu.id = 'methodblock1';
-        document.getElementById("formethod").appendChild(clonedMenu);
-        }
-
-    window.onload = loadmethod;
+window.onload = loadmethod;
 
 
-    function changetextarea() {
-    	alert("hi1");
-		let textarea = document.getElementById( 'hiddeneditor' );
-		let editor = document.getElementById( 'editor' );
-		alert("hi2");
-    	textarea.value = editor.innerHTML();
-	}
 
-	function test() {
-    	alert(editor.getData());
-    	document.getElementById( 'hiddeneditor' ).value = editor.getData();
+function changetextarea() {
+	alert("hi1");
+	let textarea = document.getElementById( 'hiddeneditor' );
+	let editor = document.getElementById( 'editor' );
+	alert("hi2");
+	textarea.value = editor.innerHTML();
+}
 
-	}
+function test() {
+	alert(editor.getData());
+	document.getElementById( 'hiddeneditor' ).value = editor.getData();
 
-	function addmet() {
-    	editor.model.change( writer => {
-		    const insertPosition = editor.model.document.selection.getFirstPosition();
-		    writer.insertText( 'Method 1 : ', {bold:true}, insertPosition);
-		} );
-	}
+}
+
+function addmet() {
+	// ADD HTML TO CKEDITOR
+	const viewFragment = editor.data.processor.toView( "<br><strong>Method : </strong><br>" );
+	const modelFragment = editor.data.toModel( viewFragment );
+
+	editor.model.insertContent( modelFragment, editor.model.document.selection );
+
+}
 
 
 </script>
 
 <div class="container-fluid">
-      <div class="row">
-        <nav class="col-md-2 d-none d-md-block sidebar">
-         
-        </nav>
+	<div class="row">
+		<nav class="col-md-2 d-none d-md-block sidebar">
 
-        <main role="main" class="col-md-8">
-          <form action="insertpost.php" method="post">
-          		<div class="d-flex justify-content-between flex-wrap align-items-center pt-1 pb-2 mb-3 border-bottom">
-		            	<input id="input" name="post_title" class="form-control inputboxes" id="input" placeholder="Title" autocomplete="off">
-				
-		          </div>
+		</nav>
 
+		<main role="main" class="col-md-8">
+			<form action="insertpost.php" method="post">
+				<div class="d-flex justify-content-between flex-wrap align-items-center pt-1 pb-2 mb-3 border-bottom">
+					<input id="input" name="post_title" class="form-control inputboxes" id="input" placeholder="Title" autocomplete="off">
 
-			    <div class="editor ck-content" id="editor" name="editor">
-			    </div>
-
-			    <textarea name="hiddeneditor" id="hiddeneditor" style="display: none;">hhh</textarea>
-
-			    <script>
-			    	
-
-			        BalloonEditor
-			            .create( document.getElementById( 'editor' ), {
-					        placeholder: 'Start your article here...'
-					    } )
-			            .then( editor => { window.editor = editor } );
-
-
-			    </script>
-
-				<a onclick="addmet()" class="btn btn-primary text-light">add method</a>
-
-			
-
-
-        </main>
-
-		<div class="container col-md-2 pt-4">
-			<div class="documentationcard">
-	          	<div class="card mx-auto" style="justify-content: center;">
-				  <div class="card-body mx-auto " style="height: 50px; padding-top: 15px;">
-				    <h6 class="text-muted mx-auto">
-	                Get some tips!<i class="far fa-lightbulb fa-fw"></i>
-	            	</h6>
-				  </div>
 				</div>
-	        </div>
-
-	        <div class="pt-3">
-	        	<button onclick="test()" class="btn btn-lg btn-success btn-block">Upload this fix</button>
-	        </div>
-      </div>
-
-      </form>
-    </div>
 
 
-<header class="page-header header container-fluid">
-</header>
+				<div class="editor ck-content" id="editor" name="editor">
+				</div>
+
+				<textarea name="hiddeneditor" id="hiddeneditor" style="display: none;">hhh</textarea>
+
+				<script>
+
+
+					BalloonEditor
+					.create( document.getElementById( 'editor' ), {
+						placeholder: 'Start your article here...',
+						image: {
+							toolbar: [ 'imageTextAlternative' ]
+						}
+					} )
+					.then( editor => { 
+						window.editor = editor;
+					} );
+
+
+				</script>
+
+				<a onclick="addmet()" class="btn btn-primary text-light">Add method</a>
+
+				<textarea class="editable" id="editable">qdsqsd</textarea>
+				<script src="Editor/js/medium-editor.js"></script>
+
+				<script>
+					var elements = document.getElementsByClassName('editable'),
+						editor = new MediumEditor(elements);
+
+						if(elements){
+						 alert(elements.length);
+						}
+				</script>
+			</main>
+
+			<div class="container col-md-2 pt-4">
+				<div class="documentationcard">
+					<div class="card mx-auto" style="justify-content: center;">
+						<div class="card-body mx-auto " style="height: 50px; padding-top: 15px;">
+							<h6 class="text-muted mx-auto">
+								Get some tips!<i class="far fa-lightbulb fa-fw"></i>
+							</h6>
+						</div>
+					</div>
+				</div>
+
+				<div class="pt-3">
+					<button onclick="test()" class="btn btn-lg btn-success btn-block">Upload this fix</button>
+				</div>
+			</div>
+
+		</form>
+	</div>
+
+
+	<header class="page-header header container-fluid">
+	</header>
 
 </body>
 </html>
